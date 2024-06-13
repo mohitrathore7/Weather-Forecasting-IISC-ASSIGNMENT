ebb84cae1dfc0efd058e9a5024a0bbd95388fc5b
Mohit Rathore <rathoremohit633@gmail.com>
2024-06-13 23:59:39 +0530

    assignment done

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.linkedin.com/in/mohit-rathore-1803b3208/"
+        > Mohit Rathore</a
+      >, <a href="https://github.com/mohitrathore7?tab=repositories">Open sourced on Github.</a> 
+    </p>
+  </div>
+  );
+}
